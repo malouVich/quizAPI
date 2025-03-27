5f3de9fd74baefa561ca8ded5ccf52e4ddd1abd8
malouVich <malouvichlavrentiew@gmail.com>
2025-03-27 11:28:05 +0100

    deployment

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,6 +4,6 @@ import app.config.ApplicationConfig;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationConfig.startServer(7070);
+        ApplicationConfig.startServer(7777);
     }
 }
