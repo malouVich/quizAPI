40399a907c5f20edaec79292416b499eebfd09fd
malouVich <malouvichlavrentiew@gmail.com>
2025-05-21 20:23:42 +0200

    rettet til miljøvariabler

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationConfig.startServer(7777);
         System.out.println("🔧 CONNECTION_STR: " + System.getenv("CONNECTION_STR"));
-        System.out.println("🔧 QUIZ_DB_NAME: " + System.getenv("QUIZ_DB_NAME"));
+        System.out.println("🔧 QUIZ_DB_NAME: " + System.getenv("DB_NAME"));
 
     }
 }
